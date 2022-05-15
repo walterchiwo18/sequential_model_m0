5522c28584b5b55cc7d027e6e2ebb83dc732dc39
Walter Chiwo <waltchiwo@gmail.com>
2022-05-15 06:00:03 -0500

    Although we've downloaded all the required libraries for some reason this SVG isn't allowing us to visualize the code.

--- sequential_model_ex.py
+++ sequential_model_ex.py
@@ -1,9 +1,13 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation
+from IPython.display import SVG
+from tensorflow.keras.utils import model_to_dot
 
 model = Sequential()
 
 model.add(Dense(10, input_dim=15))
 model.add(Activation('relu'))
 
-model.summary()
+#model.summary()
+
+SVG(model_to_dot(model).create(prog='dot',format='avg'))
