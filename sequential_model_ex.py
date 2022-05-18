d8e26e106bc817b759122cbf39ff13e4f0310cf8
Walter Chiwo <waltchiwo@gmail.com>
2022-05-18 17:53:51 -0500

    The the next thing that we do is create some random dummy data with corresponding labels

--- sequential_model_ex.py
+++ sequential_model_ex.py
@@ -1,5 +1,6 @@
 from keras.models import Sequential
 from keras.layers import Dense, Activation
+import numpy as np
 #from IPython.display import SVG
 #from tensorflow.keras.utils import model_to_dot
 
@@ -9,6 +10,11 @@ model.add(Dense(10, input_dim=15,activation='relu'))
 model.add(Dense(20, activation='sigmoid'))
 
 model.compile(optimizer="rmsprop",loss= "binary_crossentropy", metrics=["accuracy"]) 
+
+#example data
+data = np.random.random(1000,100)
+label = np.random.randint(2,size=(1000,1))
+
 #model.summary()
 
 #SVG(model_to_dot(model).create(prog='dot',format='avg'))
